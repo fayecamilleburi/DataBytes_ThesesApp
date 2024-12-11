fc6a9a8478408b0d09abaaa85cf84e2f649d5282
Faye Camille Buri <152823567+fayecamilleburi@users.noreply.github.com>
2024-12-11 21:58:13 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import UnloggedMain from '.Unlogged Components/Unogged'
+
+function App() {
+  return (
+    <div>
+      <UnloggedMain />
+    </div>
+  );
+}
+
+export default App;
