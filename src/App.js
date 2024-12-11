6c05aeaa798f558711573c8b6078bf1eeb0ec779
John Keith Mercado <152822744+KeithMercado@users.noreply.github.com>
2024-12-12 03:32:17 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ForgotPassword from './Login Components/ForgotPassword';
 import Main from './Login Components/Main';
 import Librarian from './Librarian/Librarian'
-import Unlogged from './Unlogged Components/Unlogged';
 import './index.css'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Unlogged />} />
+        <Route path="/" element={<Librarian />} />
         <Route path="/forgotpassword" element={<ForgotPassword />} />
         <Route path="/login" element={<Main />} />
         <Route path="/lib" element={<Librarian />} />
