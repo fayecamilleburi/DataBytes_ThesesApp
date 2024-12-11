39f54d3e5b120257197dae401b7b3f896f95e15d
Shaina.T <151636181+Telenshaina@users.noreply.github.com>
2024-12-11 22:51:37 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      {/* Rectangle for Library Highlights */}
+      <div className="library-highlights1">Library Highlights</div>
+
+      {/* Image below Library Highlights */}
+      <img src="/library.jpg" alt="Library" className="library-image" />
+      <div className="library-text"> 
+        <div className="line1">Launch of</div>
+        <div className="line2">BookMobile</div>
+      </div>
+      <img src="/library2.jpg" alt="Library2" className="library-image2" />
+      <img src="/library3.jpg" alt="Library3" className="library-image3" />
+      <img src="/library4.jpg" alt="Library4" className="library-image4" />
+      <img src="/library5.jpg" alt="Library5" className="library-image5" />
+      
+      <div className="library-text2">
+        <div className="line3"></div>Welcome to NEU Library!</div>
+      
+      <div className="library-text3">
+        <div className="line4">Lorem ipsum dolor sit amet, consectetur </div>
+        <div className="line5">Donec  convallis  dolor  dignissim tortor </div>
+        <div className="line6">risus pellentesque.</div>
+      </div>
+
+      <div className="library-text4">
+        <div className="line3"></div>Welcome to NEU Library!</div>
+      
+      <div className="library-text5">
+        <div className="line4">Lorem ipsum dolor sit amet, consectetur </div>
+        <div className="line5">Donec  convallis  dolor  dignissim tortor </div>
+        <div className="line6">risus pellentesque.</div>
+      </div>
+
+      <div className="recent-activity">Recent Activity</div>
+      <div className="recent-activity2"></div>
+      <div className="recent-activity3"></div>
+
+      {/* Image below Recently Accessed */}
+      <img src="/thesis1.jpg" alt="Thesis1" className="thesis-image1" />
+      <div className="thesis1"> 
+        <div className="thesisline1">MDS clinical diagnostic</div>
+        <div className="thesisline2">criteria for Parkinson's </div>
+        <div className="thesisline3">disease </div>
+      </div>
+      
+      <img src="/thesis1.jpg" alt="Thesis1" className="thesis-image2" />
+      <div className="thesis2"> 
+        <div className="thesisline4">The Brain's Default</div>
+        <div className="thesisline5">Network System </div>
+      </div>
+
+      <img src="/thesis1.jpg" alt="Thesis1" className="thesis-image3" />
+      <div className="thesis3"> 
+        <div className="thesisline6">Geometry: Learning</div>
+        <div className="thesisline7">about Triangles </div>
+      </div>
+
+      <div className="recommendations-bar">Recommendations</div>
+      <div className="recommendations-bar2"></div>
+      <div className="recommendations-box1"></div>
+      <div className="recommendations-box2"></div>
+      <div className="recommendations-box3"></div>
+      <div className="recommendations-box4"></div>
+      <div className="recommendations-box5"></div>
+      <div className="recommendations-box6"></div>
+
+      <img src="/thesisreco1.png" alt="Thesisreco1" className="reco-image1" />
+      <img src="/thesisreco2.png" alt="Thesisreco2" className="reco-image2" />
+      <img src="/thesisreco3.png" alt="Thesisreco3" className="reco-image3" />
+      <img src="/thesisreco3.png" alt="Thesisreco1" className="reco-image4" />
+      <img src="/thesisreco1.png" alt="Thesisreco2" className="reco-image5" />
+      <img src="/thesisreco2.png" alt="Thesisreco3" className="reco-image6" />
+
+      <div className="header-thesis1-reco">The Brain Network</div>
+      <div className="thesis1-reco"> 
+        <div className="thesisrecoline1">Ronal B. Postura MD, MSc,</div>
+        <div className="thesisrecoline2">Daniela Berg MD,</div>
+        <div className="thesisrecoline3">Matthew Stern MD,</div>
+        <div className="thesisrecoline4"> Werner Poewe MD,C</div>
+        <div className="thesisrecoline5">First publushed: 16 October</div>
+        <div className="thesisrecoline6">2015</div>
+      </div>
+
+      <div className="header-thesis2-reco">The Heart Network</div>
+      <div className="thesis2-reco"> 
+        <div className="thesisrecoline7">Ronal B. Postura MD, MSc,</div>
+        <div className="thesisrecoline8">Daniela Berg MD,</div>
+        <div className="thesisrecoline9">Matthew Stern MD,</div>
+        <div className="thesisrecoline10"> Werner Poewe MD,C</div>
+        <div className="thesisrecoline11">First publushed: 16 October</div>
+        <div className="thesisrecoline12">2015</div>
+      </div>
+      <div className="header-thesis3-reco">The Heart Network</div>
+      <div className="thesis3-reco"> 
+        <div className="thesisrecoline13">Ronal B. Postura MD, MSc,</div>
+        <div className="thesisrecoline14">Daniela Berg MD,</div>
+        <div className="thesisrecoline15">Matthew Stern MD,</div>
+        <div className="thesisrecoline16"> Werner Poewe MD,C</div>
+        <div className="thesisrecoline17">First publushed: 16 October</div>
+        <div className="thesisrecoline18">2015</div>
+      </div>
+
+      <div className="header-thesis4-reco">The Heart Network</div>
+      <div className="thesis4-reco"> 
+        <div className="thesisrecoline13">Ronal B. Postura MD, MSc,</div>
+        <div className="thesisrecoline14">Daniela Berg MD,</div>
+        <div className="thesisrecoline15">Matthew Stern MD,</div>
+        <div className="thesisrecoline16"> Werner Poewe MD,C</div>
+        <div className="thesisrecoline17">First publushed: 16 October</div>
+        <div className="thesisrecoline18">2015</div>
+      </div>
+      <div className="header-thesis5-reco">The Heart Network</div>
+      <div className="thesis5-reco"> 
+        <div className="thesisrecoline13">Ronal B. Postura MD, MSc,</div>
+        <div className="thesisrecoline14">Daniela Berg MD,</div>
+        <div className="thesisrecoline15">Matthew Stern MD,</div>
+        <div className="thesisrecoline16"> Werner Poewe MD,C</div>
+        <div className="thesisrecoline17">First publushed: 16 October</div>
+        <div className="thesisrecoline18">2015</div>
+      </div>
+      <div className="header-thesis6-reco">The Heart Network</div>
+      <div className="thesis6-reco"> 
+        <div className="thesisrecoline13">Ronal B. Postura MD, MSc,</div>
+        <div className="thesisrecoline14">Daniela Berg MD,</div>
+        <div className="thesisrecoline15">Matthew Stern MD,</div>
+        <div className="thesisrecoline16"> Werner Poewe MD,C</div>
+        <div className="thesisrecoline17">First publushed: 16 October</div>
+        <div className="thesisrecoline18">2015</div>
+      </div>
+
+
+
+    </div>
+  );
+}
+
+export default App;
